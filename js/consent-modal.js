7f84e555ade15ff0cdab1b95b91a558ae43e3596
Helena Viher <viherhelena@gmail.com>
2024-03-22 11:19:28 +0100

    Updated custom scrollbar selectors

--- js/consent-modal.js
+++ js/consent-modal.js
@@ -319,28 +319,6 @@ function loadConsentModal() {
       display : none;
     }
 
-    /* Custom Scrollbar CSS */
-    /* Firefox */
-    * {
-      scrollbar-width: auto;
-      scrollbar-color: #dcdcdc #ffffff;
-    }
-
-    /* Chrome, Edge, and Safari */
-    *::-webkit-scrollbar {
-      width: 16px;
-    }
-
-    *::-webkit-scrollbar-track {
-      background: #ffffff;
-    }
-
-    *::-webkit-scrollbar-thumb {
-      background-color: #dcdcdc;
-      border-radius: 10px;
-      border: 3px solid #ffffff;
-    }
-
     .consent-modal--icon {
       width: 24px;
       height: 24px;
@@ -355,6 +333,29 @@ function loadConsentModal() {
       background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 15l6 -6l6 6" /></svg>');
       background-repeat: no-repeat;
     }
+
+    /* Custom Scrollbar CSS */
+    /* Firefox */
+    #consent-banner {
+      scrollbar-width: auto;
+      scrollbar-color: #dcdcdc #ffffff;
+    }
+
+    /* Chrome, Edge, and Safari */
+    #consent-banner::-webkit-scrollbar {
+      width: 16px;
+    }
+
+    #consent-banner::-webkit-scrollbar-track {
+      background: #ffffff;
+    }
+
+    #consent-banner::-webkit-scrollbar-thumb {
+      background-color: #dcdcdc;
+      border-radius: 10px;
+      border: 3px solid #ffffff;
+    }
+
 
     @media (max-width: 768px) {
       .consent-modal {
